7da7ab8e6627b5ee8802e2a66c77913f659efcc1
Rifat504 <ming470.xing@gmail.com>
2021-03-02 01:09:52 +0800

    new one added

---
+++ double-vs-tripple.js
@@ -0,0 +1,11 @@
+const num1 = 2;
+const num2 = '2';
+
+if (num1==num2){
+    console.log('true');
+}else{
+    console.log('false');
+}
+
+// '==' wont check the type but '===' will check the type.
+// javascript compare 1 with true; 0 with false;
